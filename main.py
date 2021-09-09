872ee9986459c2d884c0d59947c789f96ced2345
AcrylicMarlin <50shorty56@gmail.com>
2021-09-09 12:52:07 -0500

    Ignored cogs file for now because of lack of cogs Added code to read cogs but commented it Added first global command

--- main.py
+++ main.py
@@ -1,15 +1,152 @@
-from json import load
 import discord
 import os
 from dotenv import load_dotenv
 load_dotenv()
 
 
-bot = discord.Client()
+bot = discord.Bot()
 
 @bot.event
 async def on_ready():
-    print('Ready!')
+    print(
+'''{} is online
+I am connected to {} servers.'''.format(bot.user.name, len(bot.guilds)))
+
+
+
+@bot.slash_command(
+    name = 'ping'
+)
+async def ping(ctx):
+    await ctx.send('Pong')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Cogs aren't implemented, so this uneeded atm. Will re-add once implemented
+# for cog in os.listdir('./cogs'):
+#     if cog.endswith('.py') and not cog.startswith('_'):
+#         try:
+#             cog = f"cogs.{cog.replace('.py', '')}"
+#             bot.load_extension(cog)
+#         except Exception as e:
+#             print(f"{cog} can not be loaded:")
+#             raise e
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 bot.run(os.environ.get('TOKEN'))
